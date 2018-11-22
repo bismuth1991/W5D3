b28718ceb200905340299f0c59ba151b6d01101a
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-11-21 16:43:55 -0800

    Start Session class for MiddleWare Project

--- skeleton/lib/session.rb
+++ skeleton/lib/session.rb
@@ -1,19 +1,25 @@
 require 'json'
+require 'byebug'
 
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    @req = req
+    @cookie = JSON.parse(@req.cookies['_rails_lite_app'])
   end
 
   def [](key)
+    @cookie[key]
   end
 
   def []=(key, val)
+    @cookie[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.set_cookie(@cookie.to_json)
   end
 end
